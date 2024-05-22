b880b7b099b6ee8b070f62be9c52c776ff20c819
Nicholas Newman <33791854+NickNewman5402@users.noreply.github.com>
2024-05-22 01:04:00 +0000

    copied over all work from MSVC. litterally copied and pasted. In this commit also I have commented out File IO as I was trying to add it for easier test casses. This should be removed before final submit. I confirmed with the teacher today 5/21/24 it should not be file ip

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "leak_detector_c.h"
 
-
 typedef struct student
 {
   char *lname; //this will require DMA to store a string
@@ -12,21 +11,19 @@ typedef struct student
   int *quizzes;//this will require DMA to store quizzes
 }student;
 
-void release_memory(student **st, int C, int N, int M);
-
 student **readCourses(int C, int N, int M) {
-  printf("%d %d %d\n", C, N, M);
-  student **student2Ptr = (student**)malloc(C * sizeof(student*)); /* This makes an array of C boxes called courses and the C boxes are large
+  //printf("%d %d %d\n", C, N, M);
+  student **courses = (student**)malloc(C * sizeof(student*)); /* This makes an array of C boxes called courses and the C boxes are large
                                   enough to hold the addresses of N student pointer... courses point to student*/
 
   for (int i = 0; i < C; i++) {
-    student2Ptr[i] = (student*)malloc(N * sizeof(student));/*This is pointing each course box to an address holding student data*/
+    courses[i] = (student*)malloc(N * sizeof(student));/*This is pointing each course box to an address holding student data*/
 
     for (int j = 0; j < N; j++) {
-      student2Ptr[i][j].quizzes = (int*)malloc(M * sizeof(int));/*This is making an array of M quizzes pointed to by student j 
+      courses[i][j].quizzes = (int*)malloc(M * sizeof(int));/*This is making an array of M quizzes pointed to by student j 
                                    pointed to by course i*/
 
-      student2Ptr[i][j].lname = (char*)malloc(50 * sizeof(char));/*This is making an array of 50 char boxes and the base address 
+      courses[i][j].lname = (char*)malloc(50 * sizeof(char));/*This is making an array of 50-char boxes and the base address 
                                    is being pointed to by student j pointed to by course i*/
 
     }// End of student loop generating lname and quizzes array
@@ -36,81 +33,128 @@ student **readCourses(int C, int N, int M) {
   for (int i = 0; i < C; i++) {// Itterating through each course
     printf("\n\t\t\t----------\n\t\t\t Course %d\n\t\t\t----------", i+1);
     for (int j = 0; j < N; j++) {// Itterating through each student
-      student2Ptr[i][j].total = 0;
+      courses[i][j].total = 0;
       printf("\nStudent %d\n", j+1);
       printf("Enter the name of the student: ");
-      scanf("%s", student2Ptr[i][j].lname);
+      scanf("%s", courses[i][j].lname);
       printf("Enter the assignment score: ");
-      scanf(" %d", &student2Ptr[i][j].assignment);
+      scanf(" %d", &courses[i][j].assignment);
       printf("Enter the quiz grade(s) for this student: ");
-      for (int k = 0; k < M; k++) {// Itterating through each students quizzes
-        scanf("%d", &student2Ptr[i][j].quizzes[k]);
-        student2Ptr[i][j].total += student2Ptr[i][j].quizzes[k];// beginning the summing of the total for course i and student j
+      for (int k = 0; k < M; k++) {// Itterating through quizzes
+        scanf("%d", &courses[i][j].quizzes[k]);
+        courses[i][j].total += courses[i][j].quizzes[k];// beginning the summing of the total for course i and student j
       }// end of quizzes loop
       printf("Enter the final exam score: ");
-      scanf(" %d", &student2Ptr[i][j].finalExam);
+      scanf(" %d", &courses[i][j].finalExam);
 
-      student2Ptr[i][j].total += student2Ptr[i][j].assignment + student2Ptr[i][j].finalExam; // adding assignment grade and final exam grade to the total
+      courses[i][j].total += courses[i][j].assignment + courses[i][j].finalExam; // adding assignment grade and final exam grade to the total
     }// end of student loop
- }// End of course loop 
+
+
+  }// End of course loop 
 
 
 
-  return student2Ptr; // returning base address of courses array to courseList in the main function
+  return courses; // returning base address of courses array to courseList in the main function
+
+}// Closes readCourses function
+
+// Working on implementing this 9:40pm 5/19/24
+student **readCoursesF(int C, int N, int M) {
+  printf("%d %d %d\n", C, N, M);
+  student **courses = (student**)malloc(C * sizeof(student*)); /* This makes an array of C boxes called courses and the C boxes are large
+                                  enough to hold the addresses of N student pointer... courses point to student*/
+
+  for (int i = 0; i < C; i++) {
+    courses[i] = (student*)malloc(N * sizeof(student));/*This is pointing each course box to an address holding student data*/
+
+    for (int j = 0; j < N; j++) {
+      courses[i][j].quizzes = (int*)malloc(M * sizeof(int));/*This is making an array of M quizzes pointed to by student j
+                                   pointed to by course i*/
+
+      courses[i][j].lname = (char*)malloc(50 * sizeof(char));/*This is making an array of 50-char boxes and the base address
+                                   is being pointed to by student j pointed to by course i*/
+
+    }// End of student loop generating lname and quizzes array
+  }// End of courses loop generating students
+
+
+  FILE *fPtr;
+  if ((fPtr = fopen("sample_in.txt", "r")) == NULL) {
+    printf("Error! opening file");
+
+    // Program exits if the file pointer returns NULL.
+    exit(1);
+  }
+
+
+  for (int i = 0; i < C; i++) {// Itterating through each course
+    printf("\n\t\t\t----------\n\t\t\t Course %d\n\t\t\t----------", i + 1);
+    for (int j = 0; j < N; j++) {// Itterating through each student
+      courses[i][j].total = 0;
+      printf("\nStudent %d\n", j + 1);
+      printf("Enter the name of the student: ");
+      scanf("%s", courses[i][j].lname);
+      printf("Enter the assignment score: ");
+      scanf(" %d", &courses[i][j].assignment);
+      printf("Enter the quiz grade(s) for this student: ");
+      for (int k = 0; k < M; k++) {// Itterating through quizzes
+        scanf("%d", &courses[i][j].quizzes[k]);
+        courses[i][j].total += courses[i][j].quizzes[k];// beginning the summing of the total for course i and student j
+      }// end of quizzes loop
+      printf("Enter the final exam score: ");
+      scanf(" %d", &courses[i][j].finalExam);
+
+      courses[i][j].total += courses[i][j].assignment + courses[i][j].finalExam; // adding assignment grade and final exam grade to the total
+    }// end of student loop
+
+
+  }// End of course loop 
+
+
+
+  return courses; // returning base address of courses array to courseList in the main function
 
 }// Closes readCourses function
 
 void printHighestTotal(student **courseList, int C, int N, int M) {
   int max = 0;
-  for (int i = 0; i < C; i++) {// Itterating through each course
-    for (int j = 0; j < N; j++) {// Itterating through each student in each course
-      if (courseList[i][j].total > max) {// Finding the highest total. If course-student-total is greater than max
-        max = courseList[i][j].total;// Then max is set to course-student-total
+  for (int i = 0; i < C; i++) {
+    for (int j = 0; j < N; j++) {
+      if (courseList[i][j].total > max) {
+        max = courseList[i][j].total;
       }
     }
-  }// Ends course-student-total itteration for finding max number
+  }
 
-  for (int i = 0; i < C; i++) {// Itterating through each course
-    for (int j = 0; j < N; j++) {// Itterating through each student in each course
-      if (max == courseList[i][j].total) {// If course-student-total is equal to max, print out this students info. 
+  for (int i = 0; i < C; i++) {
+    for (int j = 0; j < N; j++) {
+      if (max == courseList[i][j].total) {
         printf("\nName: %s\nAssignment: %d\nquizzes: ", courseList[i][j].lname, courseList[i][j].assignment);
-        for (int k = 0; k < M; k++) {// itterate through course-student-quizzes
+        for (int k = 0; k < M; k++) {
           printf("%d ", courseList[i][j].quizzes[k]);
         }
-        printf("\nFinal exam: %d\nTotal: %d\nCourse number: %d\n", courseList[i][j].finalExam, courseList[i][j].total, i+1);
-
+        printf("\nFinal exam: %d\nTotal: %d\nCourse number: %d\n", courseList[i][j].finalExam, courseList[i][j].total, i+1);					
       }
     }
-  }// Closes out course-student-total itteration for printing highest total
+  }
 
 }
 
+void release_memory(student **st, int C, int N, int M) {
 
-
-
-void release_memory(student **st, int C, int N, int M){
-
-  printf("In release memory\n");
-  
   for (int i = 0; i < C; i++) {// Itterating through each course
     for (int j = 0; j < N; j++) {// Itterating through each student in each course
-      for(int k = 0; k < M; k++){// Itterating through each student's quizzes
+      for (int k = 0; k < M; k++) {// Itterating through each student's quizzes
         free(&st[i][j].quizzes[k]);
         free(&st[i][j].lname[k]);
       }
       free(st[i]);
     }
     free(st);
-}
-printf("Exiting program\n");
+  }
+  printf("Exiting program\n");
 }// exit release_memory function
-
-
-//This is just a test for github firt test didnt work so now i am tryinh another...
-
-
-
-
 
 
 int main()
@@ -118,22 +162,68 @@ int main()
   atexit(report_mem_leak); //for memory leak detector.
 
   student **courseList;
+  int choice = 0;
+  int *C, *N, *M;
 
-  int C, N, M;
+  /* Working on this 9:40pm 5/19/24
+  printf("Do you want File IO (1) or Standard IO(2)?: ");
+  scanf("%d", &choice);
 
-  printf("Enter the number of courses: ");
-  scanf("%d", &C);
-  printf("Enter the number of students: ");
-  scanf("%d", &N);
-  printf("Enter the number of quizzes: ");
-  scanf("%d", &M);
+  selection:
+  if (choice == 1) {
+    /*##############################################################
 
-  //passing reference of C, N, and M so that we get to know what we have in the first line
-  courseList = readCourses(C, N, M);// recieving the base address of courses from readCourses function
-  printHighestTotal(courseList, C, N, M);
-  release_memory(courseList, C, N, M);
-  free(courseList);
-  printf("\nmemory cleared");
+                File IO module
+
+    ################################################################
+
+    FILE *fPtr;
+    if ((fPtr = fopen("sample_in.txt", "r")) == NULL) {
+      printf("Error! opening file");
+
+      // Program exits if the file pointer returns NULL.
+      exit(1);
+    }
+
+    fscanf(fPtr, "%d%d%d", &C, &N, &M);
+    printf("\n%d %d %d\n", C, N, M);
+
+    courseList = readCoursesF(C, N, M);// recieving the base address of courses from readCourses function
+    printHighestTotal(courseList, C, N, M);
+    release_memory(courseList, C, N, M);
+    printf("\nBack in main");
+    //free(&courseList);
+    printf("\nMemory is clear\n");
+
+  }
+  else(choice == 2); {
+    /*###############################################################
+
+              standard input module
+
+    #################################################################*/
+
+
+    printf("Enter the number of courses: ");
+    scanf("%d", &C);
+    printf("Enter the number of students: ");
+    scanf("%d", &N);
+    printf("Enter the number of quizzes: ");
+    scanf("%d", &M);
+
+    //passing reference of C, N, and M so that we get to know what we have in the first line
+    courseList = readCourses(C, N, M);// recieving the base address of courses from readCourses function
+    printHighestTotal(courseList, C, N, M);
+    release_memory(courseList, C, N, M);
+    printf("\nBack in main");
+    //free(&courseList);
+    printf("\nMemory is clear\n");
+  //}
+
+  /*if (choice != 1 || choice != 2) {
+    printf("Try again! Please choose 1 or 2");
+    goto selection;
+  }*///Working on implementing this 9:40pm 5/19/24
 
   return 0;
 }
